38f37388ae81b3c3b7313367b3185c59bde8d8b8
BWZ-CODE <mmige24@gmail.com>
2024-10-05 01:22:53 -0500

    Se finaliza version preliminar de pagina web

--- contenido.js
+++ contenido.js
@@ -295,10 +295,13 @@ function toggleSeguidores(){
     }
     
     info10.classList.toggle('inactive');
+    fondo10.classList.toggle('blur');
 }
 
 function cerrarSeguidores(){
     info10.classList.add('inactive');
+    fondo10.classList.toggle('blur')
+
 }
 
 function togglePublicaciones(){
@@ -313,9 +316,11 @@ function togglePublicaciones(){
     }
     
     info11.classList.toggle('inactive');
+    fondo11.classList.toggle('blur')
 }
 function cerrarpublicaciones(){
     info11.classList.add('inactive');
+    fondo11.classList.toggle('blur');
 }
 
 function toggleInteracciones(){
@@ -330,8 +335,172 @@ function toggleInteracciones(){
     }
     
     info12.classList.toggle('inactive');
+    fondo12.classList.toggle('blur')
 }
 
 function cerrarinteracciones(){
     info12.classList.add('inactive');
+    fondo12.classList.toggle('blur');
 }
+
+/////////////////////////////////////////////////////
+const img1_1 = document.querySelector("#img1");
+const info1_1 = document.querySelector(".card1");
+const fondo1_1 = document.querySelector(".fondo1")
+const close1_1= document.querySelector(".flechita-atras1");
+
+img1_1.addEventListener("click",abrirInfo1)
+close1_1.addEventListener("click",cerrarInfo1)
+
+function abrirInfo1(){
+    info1_1.classList.toggle("inactive")
+    fondo1_1.classList.add("blur")
+}
+function cerrarInfo1(){
+    info1_1.classList.add("inactive")
+    fondo1_1.classList.toggle("blur")
+}
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////
+const img2_2 = document.querySelector("#img2");
+const info2_2 = document.querySelector(".card2");
+const fondo2_2 = document.querySelector(".fondo1")
+const close2_2 = document.querySelector(".flechita-atras2");
+
+img2_2.addEventListener("click",abrirInfo2)
+close2_2.addEventListener("click",cerrarInfo2)
+
+function abrirInfo2(){
+    info2_2.classList.toggle("inactive")
+    fondo2_2.classList.add("blur")
+}
+function cerrarInfo2(){
+    info2_2.classList.add("inactive")
+    fondo2_2.classList.toggle("blur")
+}
+////////////////////////////////////////////////////////////////////////////
+const img3_3 = document.querySelector("#img3");
+const info3_3 = document.querySelector(".card3");
+const fondo3_3 = document.querySelector(".fondo1")
+const close3_3 = document.querySelector(".flechita-atras3");
+
+img3_3.addEventListener("click",abrirInfo3)
+close3_3.addEventListener("click",cerrarInfo3)
+
+function abrirInfo3(){
+    info3_3.classList.toggle("inactive")
+    fondo3_3.classList.add("blur")
+}
+function cerrarInfo3(){
+    info3_3.classList.add("inactive")
+    fondo3_3.classList.toggle("blur")
+}
+///////////////////////////////////////////////////////
+const img4_4 = document.querySelector("#img4");
+const info4_4 = document.querySelector(".card4");
+const fondo4_4 = document.querySelector(".fondo1")
+const close4_4 = document.querySelector(".flechita-atras4");
+
+img4_4.addEventListener("click",abrirInfo4)
+close4_4.addEventListener("click",cerrarInfo4)
+
+function abrirInfo4(){
+    info4_4.classList.toggle("inactive")
+    fondo4_4.classList.add("blur")
+}
+function cerrarInfo4(){
+    info4_4.classList.add("inactive")
+    fondo4_4.classList.toggle("blur")
+}
+///////////////////////////////////////////////////////////
+const img5_5 = document.querySelector("#img5");
+const info5_5 = document.querySelector(".card5");
+const fondo5_5 = document.querySelector(".fondo1")
+const close5_5 = document.querySelector(".flechita-atras5");
+
+img5_5.addEventListener("click",abrirInfo5)
+close5_5.addEventListener("click",cerrarInfo5)
+
+function abrirInfo5(){
+    info5_5.classList.toggle("inactive")
+    fondo5_5.classList.add("blur")
+}
+function cerrarInfo5(){
+    info5_5.classList.add("inactive")
+    fondo5_5.classList.toggle("blur")
+}
+///////////////////////////////////////////////////
+const img6_6 = document.querySelector("#img6");
+const info6_6 = document.querySelector(".card6");
+const fondo6_6 = document.querySelector(".fondo1")
+const close6_6 = document.querySelector(".flechita-atras6");
+
+img6_6.addEventListener("click",abrirInfo6)
+close6_6.addEventListener("click",cerrarInfo6)
+
+function abrirInfo6(){
+    info6_6.classList.toggle("inactive")
+    fondo6_6.classList.add("blur")
+}
+function cerrarInfo6(){
+    info6_6.classList.add("inactive")
+    fondo6_6.classList.toggle("blur")
+}
+///////////////////////////////////////
+const img7_7 = document.querySelector("#img7");
+const info7_7 = document.querySelector(".card7");
+const fondo7_7 = document.querySelector(".fondo1")
+const close7_7 = document.querySelector(".flechita-atras7");
+
+img7_7.addEventListener("click",abrirInfo7)
+close7_7.addEventListener("click",cerrarInfo7)
+
+function abrirInfo7(){
+    info7_7.classList.toggle("inactive")
+    fondo7_7.classList.add("blur")
+}
+function cerrarInfo7(){
+    info7_7.classList.add("inactive")
+    fondo7_7.classList.toggle("blur")
+}
+////////////////////////////////////////////
+const img8_8 = document.querySelector("#img8");
+const info8_8 = document.querySelector(".card8");
+const fondo8_8 = document.querySelector(".fondo1")
+const close8_8 = document.querySelector(".flechita-atras8");
+
+img8_8.addEventListener("click",abrirInfo8)
+close8_8.addEventListener("click",cerrarInfo8)
+
+function abrirInfo8(){
+    info8_8.classList.toggle("inactive")
+    fondo8_8.classList.add("blur")
+}
+function cerrarInfo8(){
+    info8_8.classList.add("inactive")
+    fondo8_8.classList.toggle("blur")
+}
+///////////////////////////////////////////////
+const img9_9 = document.querySelector("#img9");
+const info9_9 = document.querySelector(".card9");
+const fondo9_9 = document.querySelector(".fondo1")
+const close9_9 = document.querySelector(".flechita-atras9");
+
+img9_9.addEventListener("click",abrirInfo9)
+close9_9.addEventListener("click",cerrarInfo9)
+
+function abrirInfo9(){
+    info9_9.classList.toggle("inactive")
+    fondo9_9.classList.add("blur")
+}
+function cerrarInfo9(){
+    info9_9.classList.add("inactive")
+    fondo9_9.classList.toggle("blur")
+}
+
+////////////////////////////
