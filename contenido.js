3fce7dd618979e082a396e4bee405ea883b1b31f
BWZ-CODE <mmige24@gmail.com>
2024-10-11 23:29:57 -0500

    Se modifica version cel

--- contenido.js
+++ contenido.js
@@ -6,23 +6,23 @@ let items = document.querySelectorAll('.slider .item');
     function loadShow(){
         let stt = 0;
         items[active].style.transform = `none`;
-        items[active].style.zIndex = 1;
+        items[active].style.zIndex = -1;
         items[active].style.filter = 'none';
-        items[active].style.opacity = 1;
+        items[active].style.opacity = 2;
         for(var i = active + 1; i < items.length; i++){
             stt++;
             items[i].style.transform = `translateX(${120*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(-1deg)`;
             items[i].style.zIndex = -stt;
             items[i].style.filter = 'blur(5px)';
-            items[i].style.opacity = stt > 2 ? 0 : 0;
+            items[i].style.opacity = stt > 2 ? 0 : 0.5;
         }
         stt = 0;
         for(var i = active - 1; i >= 0; i--){
             stt++;
             items[i].style.transform = `translateX(${-120*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(1deg)`;
             items[i].style.zIndex = -stt;
-            items[i].style.filter = 'blur(5px)';
-            items[i].style.opacity = stt > 2 ? 0 : 0;
+            items[i].style.filter = 'blur(8px)';
+            items[i].style.opacity = stt > 2 ? 0 : 0.5;
         }
     }
     loadShow();
